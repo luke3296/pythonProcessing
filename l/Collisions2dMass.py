88cce0a7e22048b9b6b20b9fbb61a147b41ca55f
sluke2210 <sluke2210@gmail.com>
2020-08-07 00:02:12 +0100

    Update Collisions2dMass.py

--- l/Collisions2dMass.py
+++ l/Collisions2dMass.py
@@ -4,12 +4,14 @@ balls = [];
 size_ = 15;
 radiusFactor = 0.5;
 arrowSize = 3.5;
+timeStep = 5;
 
 def collided(x,y,x2,y2,m1,m2):
     if( (((x-x2)**2 + (y-y2)**2) - 100 ) <=  radiusFactor*((m1**2)+(m2**2))   <= ((x-x2)**2 + (y-y2)**2) + 100): #this is not good edge detection
         return True;
     else:
         return False;
+    
 def reset():
     balls = [];
     setup();
@@ -37,8 +39,8 @@ class ball:
         if self.y < 0:
             self.y = height-1;
         
-        self.x = self.x + self.xspeed;
-        self.y =self.y + self.yspeed;
+        self.x = self.x + self.xspeed/timeStep;
+        self.y =self.y + self.yspeed/timeStep;
         
     def display(self):
         
@@ -50,7 +52,16 @@ class ball:
         popStyle();
         
         
-
+def newV(a,b):
+    #for a
+    v1x = ( ((a.mass - b.mass)/(a.mass+b.mass))*a.xspeed  +  ((2*b.mass)/(a.mass+b.mass))*b.xspeed); 
+    v1y = ( ((a.mass - b.mass)/(a.mass+b.mass))*a.yspeed  +  ((2*b.mass)/(a.mass+b.mass))*b.yspeed); 
+    #for b
+    v2x=( ((2*a.mass)/(a.mass+b.mass))*a.xspeed  + ((b.mass-a.mass)/(b.mass+a.mass))*b.xspeed);
+    v2y=( ((2*a.mass)/(a.mass+b.mass))*a.yspeed  + ((b.mass-a.mass)/(b.mass+a.mass))*b.yspeed);
+    
+    return [v1x,v1y,v2x,v2y];
+    
 def setup():
     background(209);
     size(width,height);
@@ -61,35 +72,27 @@ def draw():
     background(209);
     if mousePressed:
         balls.append(ball(mouseX,mouseY,random(-5,5),random(-5,5),random(20,40),color(random(0,255),random(0,255),random(0,255))));
-        
-    for i in range(0,len(balls), +1):
-        balls[i].update();
-        balls[i].display();
+    
+    for k in range(timeStep):    
+        for i in range(0,len(balls), +1):
+            balls[i].update();
+            balls[i].display();
     
     
-        if(len(balls) > 0):
-            for j in range(len(balls)-1, 0, -1):
-                if( i != j):
-                    if(collided(balls[i].x, balls[i].y, balls[j].x, balls[j].y,balls[i].mass,balls[j].mass)):
-                        if( balls[j].mass -2 < balls[i].mass < balls[j].mass +2):
-                            temp = balls[i].xspeed;
-                            balls[i].xspeed = balls[j].xspeed;
-                            balls[j].xspeed = temp;
+            if(len(balls) > 0):
+                for j in range(len(balls)-1, 0, -1):
+                    if( i != j):
+                        if(collided(balls[i].x, balls[i].y, balls[j].x, balls[j].y,balls[i].mass,balls[j].mass)):
+                    
+                            speeds = newV(balls[i], balls[j]);
+                            balls[i].xspeed = speeds[0];
+                            balls[i].yspeed = speeds[1];
                         
-                            temp = balls[i].yspeed;
-                            balls[i].yspeed = balls[j].yspeed;
-                            balls[j].yspeed = temp;
-                        else:
-                            #this will eventually do something different, like calulate the new velociteis based on the mass
-                            temp = balls[i].xspeed;
-                            balls[i].xspeed = balls[j].xspeed;
-                            balls[j].xspeed = temp;
-                        
-                            temp = balls[i].yspeed;
-                            balls[i].yspeed = balls[j].yspeed;
-                            balls[j].yspeed = temp;
+                            balls[j].xspeed = speeds[2];
+                            balls[j].yspeed = speeds[3];
+                    
+                      
                         #collisions = collisions + 1;
                             print("collisions");
-                        
 
         
