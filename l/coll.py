ac9cda3eb1672d9ca7b6fdecc5b2477047a1e6a2
sluke2210 <sluke2210@gmail.com>
2020-08-06 01:07:04 +0100

    Update coll.py
    updated

--- l/coll.py
+++ l/coll.py
@@ -1,27 +1,27 @@
-
-width = 1400;
+width = 800;
 height = 800;
 balls = [];
-ballPos = [];
-size_ = 25;
-def collided(x,y,x2,y2):
-    if(((x-x2)**2 + (y-y2)**2) <= 2*(size_**2)):
+size_ = 15;
+radiusFactor = 0.5;
+
+def collided(x,y,x2,y2,m1,m2):
+    if( (((x-x2)**2 + (y-y2)**2) - 100 ) <=  radiusFactor*((m1**2)+(m2**2))   <= ((x-x2)**2 + (y-y2)**2) + 100): #this is not good edge detection
         return True;
     else:
         return False;
 def reset():
     balls = [];
+    setup();
     
 class ball:
-    #global balls; 
-    #global ballPos;
-    def __init__(self, xpos,ypos,xspeed,yspeed,col):
+
+    def __init__(self, xpos,ypos,xspeed,yspeed,mass,col):
         self.x = xpos;
         self.y = ypos;
         self.xspeed = xspeed;
         self.yspeed = yspeed;
+        self.mass = mass;
         self.col = col;
-        #self.index = len(balls);
         
         
     def update(self):
@@ -36,22 +36,22 @@ class ball:
         
         self.x = self.x + self.xspeed;
         self.y =self.y + self.yspeed;
-        #ballPos[self.index] = [self.x + self.xspeed, self.y + self.yspeed];
         
     def display(self):
         fill(self.col);
-        #rectMode(CENTER);
-        ellipse(self.x, self.y , size_,size_);
-a = ball(0,0,5,5,color(12,23,34));
+        ellipse(self.x, self.y , self.mass,self.mass);
+        
 
 def setup():
     background(209);
     size(width,height);
-    frameRate(20);
+    frameRate(15);
+    
 def draw():
+
     background(209);
     if mousePressed:
-        balls.append(ball(mouseX,mouseY,random(-5,5),random(-5,5),color(random(0,255),random(0,255),random(0,255))));
+        balls.append(ball(mouseX,mouseY,random(-5,5),random(-5,5),random(10,30),color(random(0,255),random(0,255),random(0,255))));
         
     for i in range(0,len(balls), +1):
         balls[i].update();
@@ -61,15 +61,15 @@ def draw():
         if(len(balls) > 0):
             for j in range(len(balls)-1, 0, -1):
                 if( i != j):
-                    if(collided(balls[i].x, balls[i].y, balls[j].x, balls[j].y)):
+                    if(collided(balls[i].x, balls[i].y, balls[j].x, balls[j].y,balls[i].mass,balls[j].mass)):
                         balls[i].xspeed = balls[i].xspeed  *-1;
                         balls[j].xspeed  = balls[j].xspeed  *-1;
                         balls[i].yspeed  = balls[i].yspeed  *-1;
                         balls[j].yspeed  = balls[j].yspeed  *-1;
-                        print("collided");
+                        #collisions = collisions + 1;
+                        print("collisions");
                         
     
     if keyPressed and keyCode == SHIFT:
         print("reset");
         reset();
-        setup();
